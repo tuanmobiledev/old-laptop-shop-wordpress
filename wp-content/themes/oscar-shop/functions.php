3f69f5b23c4a8780bd3f796d9592434f03336531
Boss <maytinhoscar@gmail.com>
2026-08-30 06:05:26 +0000

    fix(P0-3): also update JS hash reference DJKA3jcu → CGMcENqJ (regex miss line 62)

--- wp-content/themes/oscar-shop/functions.php
+++ wp-content/themes/oscar-shop/functions.php
@@ -59,7 +59,7 @@ function oscar_shop_enqueue_assets(): void
     // makes @font-face and module preload URLs absolute under theme folder.
 wp_enqueue_script(
         'oscar-storefront',
-        get_template_directory_uri() . '/assets/index-DJKA3jcu.js',
+        get_template_directory_uri() . '/assets/index-CGMcENqJ.js',
         [],
         null,
         true
