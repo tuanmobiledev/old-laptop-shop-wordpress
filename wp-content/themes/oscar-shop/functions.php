6d80ea5fa2ffe599383a40ab55ca491ba2dfbeb8
Boss <maytinhoscar@gmail.com>
2026-09-03 23:39:35 +0700

    Revert "feat(deploy): bump bundle JS DVycUl6e → CfY-CliT + title update (Boss 2026-09-03)"
    This reverts commit 668c1a51d1d5d16a14ebc56d1a45104da7e413d5.

--- wp-content/themes/oscar-shop/functions.php
+++ wp-content/themes/oscar-shop/functions.php
@@ -59,7 +59,7 @@ function oscar_shop_enqueue_assets(): void
     // makes @font-face and module preload URLs absolute under theme folder.
 wp_enqueue_script(
         'oscar-storefront',
-        get_template_directory_uri() . '/assets/index-CfY-CliT.js',
+        get_template_directory_uri() . '/assets/index-DVycUl6e.js',
         [],
         null,
         true
